4456559c4579534fa2d870369bb6e4464dcb9b1f
이효주 <dlgywn2420@gmail.com>
2024-06-30 18:34:00 +0900

    리팩토링

--- src/Bank.java
+++ src/Bank.java
@@ -2,12 +2,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bank {
-    private List<Account> accounts = new ArrayList<>();
+    private List<Account> accounts;
     private int totalAccount;
     private static Bank bank;
 
-
     public Bank() {
+        this.accounts = new ArrayList<>();
     }
 
     public static Bank getInstance() {
@@ -19,36 +19,26 @@ public class Bank {
 
     public void addAccount(String accountNo, String name) {
         accounts.add(new Account(accountNo, name));
+        totalAccount++;
     }
 
-    public Account getAccount(String accountNo) {
-        for (Account account : accounts) {
-            if (account.getAccountNo().equals(accountNo)) {
-                return account;
-            }
-        }
-        System.out.println("계좌가 없습니다.");
+    public List<Account> getAccounts() {
+        return accounts;
+    }
+
+    public Account getAccount() {
         return null;
-    }
-
-    public List<Account> findAccounts(String name) {
-        List<Account> userAccounts = new ArrayList<>();
-        for (Account account : accounts) {
-            if (account.getName().equals(name))
-                userAccounts.add(account);
-        }
-        return userAccounts;
-    }
-
-    public int getTotalAccount(int totalAccount) {
-        return totalAccount;
     }
 
     public int getTotalAccount() {
         return totalAccount;
     }
 
-    public List<Account> getAccounts() {
-        return accounts;
+    @Override
+    public String toString() {
+        return "Bank{" +
+                "accounts=" + accounts +
+                ", totalAccount=" + totalAccount +
+                '}';
     }
 }
